93842293aac6b50d9d48e60168fcd3ce3b98dd76
Lucas Rocha <rocha.lucas20@gmail.com>
2018-06-14 13:41:39 +0000

    Arrumando os tabs do move.js

--- js/move.js
+++ js/move.js
@@ -5,58 +5,45 @@
  * saida: Assuntos com o nome para busca.
  */
 function Api() {
-         var cx = '007833354045512050991:kcbhcpzkuai';
-         var gcse = document.createElement('script');
-         gcse.type = 'text/javascript';
-         gcse.async = true;
-         gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
-         var s = document.getElementsByTagName('script')[0];
-         s.parentNode.insertBefore(gcse, s);
-         };
+    var cx = '007833354045512050991:kcbhcpzkuai';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+}
+
 /*
  * descrição: Slide de imagens
  * autor: Allan Silva Bezerra
  * entrada:  
  * saida: 
  */
- 
- 
-     var slideIndex = 1;
-    showDivs(slideIndex);
+var slideIndex = 1;
+showDivs(slideIndex);
 
-    function plusDivs(n) {
+function plusDivs(n) {
     showDivs(slideIndex += n);
-    }
+}
 
-    function currentDiv(n) {
+function currentDiv(n) {
     showDivs(slideIndex = n);
-    }
+}
 
-    function showDivs(n) {
+function showDivs(n) {
     var i;
     var x = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("demo");
-        if (n > x.length) {slideIndex = 1}    
-            if (n < 1) {slideIndex = x.length}
-                for (i = 0; i < x.length; i++) {
-                    x[i].style.display = "none";  
-                }
-                for (i = 0; i < dots.length; i++) {
-                    dots[i].className = dots[i].className.replace(" w3-white", "");
-                }
-                x[slideIndex-1].style.display = "block";  
-                dots[slideIndex-1].className += " w3-white";
-                }
+    if (n > x.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" w3-white", "");
+    }
+    x[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " w3-white";
+}
                 
-// Here's my data model
-var ViewModel = function(first, last) {
-    this.firstName = ko.observable(first);
-    this.lastName = ko.observable(last);
-
-    this.fullName = ko.pureComputed(function() {
-        // Knockout tracks dependencies automatically. It knows that fullName depends on firstName and lastName, because these get called when evaluating fullName.
-        return this.firstName() + " " + this.lastName();
-    }, this);
-};
-
-ko.applyBindings(new ViewModel("Planet", "Earth")); // This makes Knockout get to work
