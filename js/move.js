d96993ef9fad4f3c98e387e76bfc8dd0b68801a2
Allan Silva Bezerra <silbez.allan@gmail.com>
2018-06-14 02:55:55 +0000

    Transferencia de funções para arquivo .js / Exclusão de arquivos desnecessários

--- js/move.js
+++ js/move.js
@@ -1,11 +1,10 @@
-
-    /*
-     * descrição: API Google Search
-     * autor: Juan Bertoluzzi Garcia
-     * entrada: Nome para busca.
-     * saida: Assuntos com o nome para busca.
-     */
-    function Api() {
+/*
+ * descrição: API Google Search
+ * autor: Juan Bertoluzzi Garcia
+ * entrada: Nome para busca.
+ * saida: Assuntos com o nome para busca.
+ */
+function Api() {
          var cx = '007833354045512050991:kcbhcpzkuai';
          var gcse = document.createElement('script');
          gcse.type = 'text/javascript';
@@ -14,4 +13,37 @@
          var s = document.getElementsByTagName('script')[0];
          s.parentNode.insertBefore(gcse, s);
          };
-  
+/*
+ * descrição: Slide de imagens
+ * autor: Allan Silva Bezerra
+ * entrada:  
+ * saida: 
+ */
+function slider(){
+    var slideIndex = 1;
+    showDivs(slideIndex);
+
+    function plusDivs(n) {
+    showDivs(slideIndex += n);
+    }
+
+    function currentDiv(n) {
+    showDivs(slideIndex = n);
+    }
+
+    function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+        if (n > x.length) {slideIndex = 1}    
+            if (n < 1) {slideIndex = x.length}
+                for (i = 0; i < x.length; i++) {
+                    x[i].style.display = "none";  
+                }
+                for (i = 0; i < dots.length; i++) {
+                    dots[i].className = dots[i].className.replace(" w3-white", "");
+                }
+                x[slideIndex-1].style.display = "block";  
+                dots[slideIndex-1].className += " w3-white";
+                }
+  }
