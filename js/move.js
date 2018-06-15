fd2e5a5ec831394eb755bbb197cbc02446ae5571
Lucas Rocha <rocha.lucas20@gmail.com>
2018-06-15 01:34:04 +0000

    alteracao move.js

--- js/move.js
+++ js/move.js
@@ -82,6 +82,7 @@ var ClickCounterViewModel = function() {
  * entrada: 
  * saida: 
  */
+<<<<<<< HEAD
 function colab() {
     var SimpleListModel = function(items) {
         this.items = ko.observableArray(items);
@@ -96,3 +97,56 @@ function colab() {
     
     ko.applyBindings(new SimpleListModel(["Grazi", "Janaina", "Khayan"]));
 }
+=======
+ 
+ var initialData = [
+    { firstName: "Danny", lastName: "LaRusso", phones: [
+        { type: "Mobile", number: "(555) 121-2121" },
+        { type: "Home", number: "(555) 123-4567"}]
+    },
+    { firstName: "Sensei", lastName: "Miyagi", phones: [
+        { type: "Mobile", number: "(555) 444-2222" },
+        { type: "Home", number: "(555) 999-1212"}]
+    }
+];
+ 
+
+function contato(){
+var ContactsModel = function(contacts) {
+    var self = this;
+    self.contacts = ko.observableArray(ko.utils.arrayMap(contacts, function(contact) {
+        return { firstName: contact.firstName, lastName: contact.lastName, phones: ko.observableArray(contact.phones) };
+    }));
+ 
+    self.addContact = function() {
+        self.contacts.push({
+            firstName: "",
+            lastName: "",
+            phones: ko.observableArray()
+        });
+    };
+ 
+    self.removeContact = function(contact) {
+        self.contacts.remove(contact);
+    };
+ 
+    self.addPhone = function(contact) {
+        contact.phones.push({
+            type: "",
+            number: ""
+        });
+    };
+ 
+    self.removePhone = function(phone) {
+        $.each(self.contacts(), function() { this.phones.remove(phone) })
+    };
+ 
+    self.save = function() {
+        self.lastSavedJson(JSON.stringify(ko.toJS(self.contacts), null, 2));
+    };
+ 
+    self.lastSavedJson = ko.observable("")
+};
+ 
+ko.applyBindings(new ContactsModel(initialData));}
+>>>>>>> e8d3d8c926b9a30621b2782574bcf5269ca4416e
